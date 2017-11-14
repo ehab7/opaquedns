f01f5c53fadb9e0b22c98000f890344e133371b5
ehab7 <ehab7gb@gmail.com>
2017-11-13 23:29:54 -0800

    added logging

--- opaquedns.py
+++ opaquedns.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Discrption:
-python script runs dns proxy server trying to prevent dns tunneling:
-  - keeps track of the CNAME responses query size (number of characters)
-  - slows down or cuts off responses at certain values.
-  - overwrites ttl if less than certian value.
-  - prevents queries with obsoleted type.
-  - blocks CNAME size if exceeded certian size.
-  - limits the number of CNAME's response.
-  - uppercase the CNAME response to weaken chance to inflate illegit compressed response. 
-  - whitelist/blacklist for sites.
-"""
 
 from twisted.names import dns, server, client, cache,error
 from twisted.application import service, internet
 from twisted.internet import task,reactor,defer
 import sys,re,time
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class DNSResolver(client.Resolver):
     # example for blacklisting site like anti-verus..etc
@@ -30,8 +23,8 @@ class DNSResolver(client.Resolver):
     blackListRecordType = [3,4,254,7,8,9,14,253,11,32 ,33,0,38,30 ,13,17,19,20 ,21,22,23 , \
                            26,31,32 ,34,42,40,27,100, 101,102,103,99]
     
-    # example for whitelisting site like payment..etc
-    whiteListDNS = [re.compile(".*paypal.*")]
+    # example for whitelisting site like google..etc
+    whiteListDNS = [re.compile(".*\.google\.com$")]
 
     # regEX to extract site name 
     siteRegx     = re.compile(".*\.(.*)\.(.*)")
@@ -49,7 +42,7 @@ class DNSResolver(client.Resolver):
     maxReqNameLen = 200
 
     # the cut off rate is the max allowed rate characters per second so 10kbps is around ~1250
-    maxSiteByteRate = 1250
+    maxSiteCharsRate = 1250
 
     # the slow down rate is the rate (characters) per second which response will be delayed by 5 seconds. 
     slowSiteAtByteRate = 900
@@ -70,12 +63,16 @@ class DNSResolver(client.Resolver):
     @staticmethod
     def checkWhiteList(txt):
         for each in DNSResolver.whiteListDNS:
-            if each.match(txt): return True
+            if each.match(txt):
+                logger.info("site {} is whitelisted".format(txt)) 
+                return True
 
     @staticmethod
     def checkBlackList(txt):
         for each in DNSResolver.blackListDNS:
-            if each.match(txt): return True
+            if each.match(txt):
+                logger.info("site {} is blacklisted".format(txt)) 
+                return True
 
     @staticmethod
     def checkSite(site):
@@ -86,7 +83,9 @@ class DNSResolver(client.Resolver):
 
     @staticmethod
     def updateSite(site,size):
-        if len(DNSResolver.bigDict) > 30000: return 
+        if len(DNSResolver.bigDict) > 30000:
+            logging.warn("skip updates dictionary is too big") 
+            return 
         x = DNSResolver.bigDict.get(site,0)
         if x:
             record = x.split('_')
@@ -117,7 +116,9 @@ class DNSResolver(client.Resolver):
         aRecords = [0] # use list to workaround nested func can't rebind nonlocal vars. 
         cRecords = [0]
         
-        # to slow down the suspected requests
+
+
+        # low down the suspected requests
         def slowDownResponse(f):
             return result[0],result[1],[]
 
@@ -125,14 +126,16 @@ class DNSResolver(client.Resolver):
         def processAnswer(readBack):
             count,i=readBack[0],readBack[1]
             if i >= len(result[0]):
-                if len(answers) < 1 : # in case couldn't build any answer use the org answer
+                if len(answers) < 1 : # in case couldn't build any answer use the org one
+                    logger.warn("can not build reply use the orginal answer ")
                     return result[0],result[1],[]
                 return answers, authority, additional
 
 
             answer = result[0][i]
             slowDown = False
-            if count and count > DNSResolver.maxSiteByteRate:
+            if count and count > DNSResolver.maxSiteCharsRate:
+                logger.debug("execeed the max site characters rate")
                 return defer.fail(error.DomainError())
             else:
                 if count and count > DNSResolver.slowSiteAtByteRate:
@@ -142,8 +145,6 @@ class DNSResolver(client.Resolver):
                     siteName = b"%s"%answer.name
                     siteCName = getattr(answer.payload, "name", "")
                     siteCName = b"%s"%siteCName
-                    if siteName and DNSResolver.checkWhiteList(siteName): return result
-                    if siteName and DNSResolver.checkBlackList(siteName): return  answers, authority, additional
                     if len(siteCName) > DNSResolver.maxRespNameLen: return defer.fail(error.DomainError())
 
                     siteRe = DNSResolver.siteRegx.match(siteName)
@@ -192,15 +193,11 @@ class DNSResolver(client.Resolver):
             der = DNSResolver.checkSite(domain)
             return processAnswer([der,0])
 
+        if DNSResolver.checkWhiteList(str(result[0][0].name)): return result
         return handler()      
      
     def lookupText(self,name,timeout):
         def handler(result):
-            for answer in result[0]:
-                siteName = str(answer.name)
-                if DNSResolver.checkWhiteList(siteName): return result
-                if DNSResolver.checkBlackList(siteName): return defer.fail(error.DomainError())
-            
             for authory in result[1]:
                 siteName = str(authory.name)
                 if len(siteName) > DNSResolver.maxRespNameLen: return defer.fail(error.DomainError())
@@ -210,9 +207,16 @@ class DNSResolver(client.Resolver):
         return s.addCallback(handler)
 
 
-    def query(self, query, timeout=5):   
+    def query(self, query, timeout=5):
+        # example of explicit blocking of record type PTR
+        if query.type == dns.PTR:
+            return defer.fail(error.DomainError())
+
         if query.type in DNSResolver.blackListRecordType:
             return defer.fail(error.DomainError())
+        
+        if DNSResolver.checkBlackList(b"%s"%query.name): 
+            return  defer.fail(error.DomainError())
 
         return client.Resolver.query(self,query,timeout)
 
@@ -224,14 +228,16 @@ class DNSResolver(client.Resolver):
             result.addCallback(self.specialLookUp)
             return result
 
-        
-db_dns_resolver = DNSResolver( [("8.8.8.8", 53), ("8.8.4.4", 53)] )
-f = server.DNSServerFactory(clients=[db_dns_resolver])
-p = dns.DNSDatagramProtocol(f)    
-f.noisy = p.noisy = False
 
 
-# runing a proxy on port 10053
-reactor.listenUDP(10053, p)
-reactor.listenTCP(10053, f)
-reactor.run()
+if __name__ == '__main__':
+    print "using 8.8.8.8 and 8.8.4.4 as our forwarders"
+    db_dns_resolver = DNSResolver( [("8.8.8.8", 53), ("8.8.4.4", 53)] )
+    f = server.DNSServerFactory(clients=[db_dns_resolver])
+    p = dns.DNSDatagramProtocol(f)    
+    f.noisy = p.noisy = False
+
+    # runing a proxy on port 10053
+    reactor.listenUDP(10053, p)
+    reactor.listenTCP(10053, f)
+    reactor.run()
